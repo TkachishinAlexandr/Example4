38cf2cec83f846d771e271b7e5cf4be63bb7154e
Александр <@TKACHISHINNOUT>
2020-12-22 20:19:08 +0200

    задача

---
+++ Example4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, };
+            var pow = from n in numbers.AsParallel()
+                             select Pow(n);
+            List<int> list = pow.ToList();
+                foreach (var n in list)
+                Console.WriteLine(n);
+            Console.ReadLine();
+        }
+        static int Pow(int x)
+        {
+            int result = x * x * x;
+            Console.WriteLine($"Возведение втретью степень числа {x} равно {result}");
+            return result;
+        }
+    }
+}
